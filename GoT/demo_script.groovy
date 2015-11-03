102a148c75f508661156812836d92a7fce52ee33
Lorina Poland <lorina@datastax.com>
2015-11-03 12:35:56 -0800

    adding demo_script.groovy

---
+++ GoT/demo_script.groovy
@@ -0,0 +1,24 @@
+println "Make Titan Cassandra graph"
+graph = TitanFactory.open('conf/titan-cassandra.properties')
+println "Load GoT script"
+:load /home/polandll/CLONES/graph-examples/GoT/GoT-titan-schema.groovy
+println "Run Schema function"
+defineGoTSchema(graph)
+println "Create a graph traversal object"
+g = graph.traversal(standard())
+println "Count the number of objects"
+g.V().count()
+println "Load the GraphML data file"
+graph.io(IoCore.graphml()).readGraph('/tmp/GoT2.gml')
+
+println "Find all relatedBy edges"
+g.E().has(label,'relatedBy').values()
+println "Find all killedBy edges"
+g.E().has(label,'killedBy').values()
+println "Find all Daenerys's information"
+g.V().has('hname','Daenerys').values()
+
+println "Close the graph"
+graph.close()
+println "exit gremlin"
+:quit
