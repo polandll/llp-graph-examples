2deea06b390392ec4831a0b97f3e71165157904a
Lorina Poland <lorina@datastax.com>
2015-12-18 15:34:36 -0800

    moved stuff

--- DSE_GRAPH_WORKSHOP/HULKempty-sample.groovy
+++
@@ -1,4 +0,0 @@
-// an init script that returns a Map allows explicit setting of global bindings.
-def globals = [:]
-// define the default TraversalSource to bind queries to - this one will be named "g".
-globals << [g : graph.traversal()]
