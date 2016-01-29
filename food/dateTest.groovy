0324a90442587dad49dcd080c7ea38edbd984e18
Lorina Poland <lorina@datastax.com>
2016-01-29 15:28:37 -0800

    adding date test

---
+++ food/dateTest.groovy
@@ -0,0 +1,7 @@
+johnDoe = graph.addVertex(label, 'reviewer', 'id', 400, 'revname','John Doe')
+beefBourguignon = graph.addVertex('id', 200, label, 'recipe', 'recipeTitle', 'Beef Bourguignon', 'instructions', 'Braise the beef. Saute the onions and carrots. Add wine and cook in a dutch oven at 425 degrees for 1 hour.')
+
+
+// reviewer - recipe edges
+
+johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-01T00:00:00.00Z'), 'stars', 5, 'comment', 'Pretty tasty!')
