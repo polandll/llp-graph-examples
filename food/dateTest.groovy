9e2c6ebd3f9ad3281bb19f294c087424967a38ea
Lorina Poland <lorina@datastax.com>
2016-01-29 16:14:45 -0800

    figured out date thing

--- food/dateTest.groovy
+++ food/dateTest.groovy
@@ -4,5 +4,15 @@ beefBourguignon = graph.addVertex('id', 200, label, 'recipe', 'recipeTitle', 'Be
 
 // reviewer - recipe edges
 
-//johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse(Date(99, 0, 1)'2014-01-01T00:00:00.00Z'), 'stars', 5, 'comment', 'Pretty tasty!')
-johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse(Date(99, 0, 1)), 'stars', 5, 'comment', 'Pretty tasty!')
+johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-01T00:00:00.00Z'), 'stars', 5, 'comment', 'Pretty tasty!')
+//johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse(Date(99, 0, 1)), 'stars', 5, 'comment', 'Pretty tasty!')
+
+johnDoe = graph.addVertex(label, 'reviewer', 'id', 400, 'revname','John Doe');beefBourguignon = graph.addVertex('id', 200, label, 'recipe', 'recipeTitle', 'Beef Bourguignon', 'instructions', 'Braise the beef. Saute the onions and carrots. Add wine and cook in a dutch oven at 425 degrees for 1 hour.'); johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-01T00:00:00.00Z').toEpochMilli(), 'stars', 5, 'comment', 'Pretty tasty!')
+
+
+    DateTimeFormatter ISO_DATE_TIME = new DateTimeFormatterBuilder()
+            .append(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+            .appendOffset("+HHmm", "+0000")
+            .toFormatter();
+  "'birthday', " + LocalDate.from(ISO_DATE.parse(attributes[4])).atStartOfDay().toEpochSecond(ZoneOffset.UTC) + ", " +
+ "'creationDate', " + Instant.from(ISO_DATE_TIME.parse(attributes[5])).toEpochMilli() + ", " +
