d760cde1d9dc5725011045804c352068e27e897d
Lorina Poland <lorina@datastax.com>
2016-01-29 16:16:39 -0800

    figured out date thing part 2

--- food/Gremlin/generateAll.groovy
+++ food/Gremlin/generateAll.groovy
@@ -175,10 +175,10 @@ betsyJones = graph.addVertex(label, 'reviewer', 'id', 404, 'revname','Betsy Jone
 
 // reviewer - recipe edges
 
-johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-01T00:00:00.00Z'), 'stars', 5, 'comment', 'Pretty tasty!')
-johnSmith.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-23T00:00:00.00Z'), 'stars', 4)
-janeDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-02-01T00:00:00.00Z'), 'stars', 5, 'comment', 'Yummy!')
-sharonSmith.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2015-01-01T00:00:00.00Z'), 'stars', 3, 'comment', 'It was okay.')
-johnDoe.addEdge('rated', spicyMeatloaf, 'ratedDate', Instant.parse('2015-12-31T00:00:00.00Z'), 'stars', 4, 'comment', 'Really spicy - be careful!')
-sharonSmith.addEdge('rated', spicyMeatloaf, 'ratedDate',Instant.parse('2014-07-23T00:00:00.00Z'), 'stars', 3, 'comment', 'Too spicy for me. Use less garlic.')
-janeDoe.addEdge('rated', carrotSoup, 'ratedDate', Instant.parse('2015-12-30T00:00:00.00Z'), 'stars', 5, 'comment', 'Loved this soup! Yummy vegetarian!')
+johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-01T00:00:00.00Z').toEpochMilli(), 'stars', 5, 'comment', 'Pretty tasty!')
+johnSmith.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-23T00:00:00.00Z').toEpochMilli(), 'stars', 4)
+janeDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-02-01T00:00:00.00Z').toEpochMilli(), 'stars', 5, 'comment', 'Yummy!')
+sharonSmith.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2015-01-01T00:00:00.00Z').toEpochMilli(), 'stars', 3, 'comment', 'It was okay.')
+johnDoe.addEdge('rated', spicyMeatloaf, 'ratedDate', Instant.parse('2015-12-31T00:00:00.00Z').toEpochMilli(), 'stars', 4, 'comment', 'Really spicy - be careful!')
+sharonSmith.addEdge('rated', spicyMeatloaf, 'ratedDate',Instant.parse('2014-07-23T00:00:00.00Z').toEpochMilli(), 'stars', 3, 'comment', 'Too spicy for me. Use less garlic.')
+janeDoe.addEdge('rated', carrotSoup, 'ratedDate', Instant.parse('2015-12-30T00:00:00.00Z').toEpochMilli(), 'stars', 5, 'comment', 'Loved this soup! Yummy vegetarian!')
