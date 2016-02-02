e3abe759fb472c9a563b68d13c5db0a1bda9d070
Lorina Poland <lorina@datastax.com>
2016-02-02 14:48:58 -0800

    added type casting to scripts

--- food/Gremlin/generateReviews.groovy
+++ food/Gremlin/generateReviews.groovy
@@ -3,11 +3,11 @@
 // script = new File('/Users/lorinapoland/CLONES/graph-examples/food/generateReviews.groovy').text; []
 
 // reviewer vertices
-johnDoe = graph.addVertex(label, 'reviewer', 'id', 400, 'revname','John Doe')
-johnSmith = graph.addVertex(label, 'reviewer', 'id', 401, 'revname','John Smith')
-janeDoe = graph.addVertex(label, 'reviewer', 'id', 402, 'revname','Jane Doe')
-sharonSmith = graph.addVertex(label, 'reviewer', 'id', 403, 'revname','Sharon Smith')
-betsyJones = graph.addVertex(label, 'reviewer', 'id', 404, 'revname','Betsy Jones')
+Vertex johnDoe = graph.addVertex(label, 'reviewer', 'id', 400, 'revname','John Doe')
+Vertex johnSmith = graph.addVertex(label, 'reviewer', 'id', 401, 'revname','John Smith')
+Vertex janeDoe = graph.addVertex(label, 'reviewer', 'id', 402, 'revname','Jane Doe')
+Vertex sharonSmith = graph.addVertex(label, 'reviewer', 'id', 403, 'revname','Sharon Smith')
+Vertex betsyJones = graph.addVertex(label, 'reviewer', 'id', 404, 'revname','Betsy Jones')
 
 // reviewer - recipe edges
 johnDoe.addEdge('rated', beefBourguignon, 'ratedDate', Instant.parse('2014-01-01T00:00:00.00Z'), 'stars', 5, 'comment', 'Pretty tasty!')
