8922c1b98ecbad1d06a970090b8d92484df89b57
Lorina Poland <lorina@datastax.com>
2016-02-05 12:57:42 -0800

    VertexIndex changes, DSP-7740

--- food/CSV/RecipeParserCSV.groovy
+++ food/CSV/RecipeParserCSV.groovy
@@ -61,7 +61,7 @@ class RecipeParser {
             // ********** PROBLEM HERE
             // **************************
             authors.split('\\|').each { def authId ->
-                def authVertex = g.V().has("author", "aname", authId).tryNext()
+                def authVertex = g.V().has("author", "aname", authId).tryNext().orElseGet {graph.addVertex(label, "author", "aname", authId)}
                 bookVertex.addEdge('authored', authVertex)
                 if (++counter % batchSize == 0) graph.tx().commit()
             }
@@ -74,52 +74,52 @@ class RecipeParser {
     }
          
     public static void createSchema(final DseGraph graph) {
-            //schema = graph.schema()
-        graph.migration("setup", { def schema ->
+            Schema schema = graph.schema()
+        //graph.migration("setup", { def schema ->
     	
     	    // Property Keys
-    		def id = schema.buildPropertyKey('myId', Integer.class).add()
-    		def aname = schema.buildPropertyKey('aname', String.class).add()
-    		def gender = schema.buildPropertyKey('gender', String.class).add()
-    		def revname = schema.buildPropertyKey('revname', String.class).add()
-    		def recipeTitle = schema.buildPropertyKey('recipeTitle', String.class).add()
-    		def instructions = schema.buildPropertyKey('instructions', String.class).add()
-    		def iName = schema.buildPropertyKey('iName', String.class).add()
-    		def bookTitle = schema.buildPropertyKey('bookTitle', String.class).add()
-    		def publishDate = schema.buildPropertyKey('publishDate', Integer.class).add()
-    		def ISBN = schema.buildPropertyKey('ISBN', String.class).add()
-    		def mname = schema.buildPropertyKey('mealTitle', String.class).add()
-    		def mCreateDate = schema.buildPropertyKey('mCreateDate', Instant.class).add()
-    		def calories = schema.buildPropertyKey('calories', Integer.class).add()
+    		PropertyKey id = schema.buildPropertyKey('myId', Integer.class).add()
+    		PropertyKey aname = schema.buildPropertyKey('aname', String.class).add()
+    		PropertyKey gender = schema.buildPropertyKey('gender', String.class).add()
+    		PropertyKey revname = schema.buildPropertyKey('revname', String.class).add()
+    		PropertyKey recipeTitle = schema.buildPropertyKey('recipeTitle', String.class).add()
+    		PropertyKey instructions = schema.buildPropertyKey('instructions', String.class).add()
+    		PropertyKey iName = schema.buildPropertyKey('iName', String.class).add()
+    		PropertyKey bookTitle = schema.buildPropertyKey('bookTitle', String.class).add()
+    		PropertyKey publishDate = schema.buildPropertyKey('publishDate', Integer.class).add()
+    		PropertyKey ISBN = schema.buildPropertyKey('ISBN', String.class).add()
+    		PropertyKey mname = schema.buildPropertyKey('mealTitle', String.class).add()
+    		PropertyKey mCreateDate = schema.buildPropertyKey('mCreateDate', Instant.class).add()
+    		PropertyKey calories = schema.buildPropertyKey('calories', Integer.class).add()
                 
-    		def rCreateDate = schema.buildPropertyKey('rCreateDate', Integer.class).add()
-    		def amount = schema.buildPropertyKey('amount', String.class).add()
-    		def stars = schema.buildPropertyKey('stars', Integer.class).add()
-    		def ratedDate = schema.buildPropertyKey('ratedDate', Instant.class).add()
-    		def comment = schema.buildPropertyKey('comment', String.class).add()
+    		PropertyKey rCreateDate = schema.buildPropertyKey('rCreateDate', Integer.class).add()
+    		PropertyKey amount = schema.buildPropertyKey('amount', String.class).add()
+    		PropertyKey stars = schema.buildPropertyKey('stars', Integer.class).add()
+    		PropertyKey ratedDate = schema.buildPropertyKey('ratedDate', Instant.class).add()
+    		PropertyKey comment = schema.buildPropertyKey('comment', String.class).add()
     		
     		// Vertex Labels
-    		def author = schema.buildVertexLabel('author').add()
-    		def recipe = schema.buildVertexLabel('recipe').add()
-			def ingredient = schema.buildVertexLabel('ingredient').add()
-    		def book = schema.buildVertexLabel('book').add()
-    		def meal = schema.buildVertexLabel('meal').add()
-    		def reviewer = schema.buildVertexLabel('reviewer').add()
+    		VertexLabel author = schema.buildVertexLabel('author').add()
+    		VertexLabel recipe = schema.buildVertexLabel('recipe').add()
+			VertexLabel ingredient = schema.buildVertexLabel('ingredient').add()
+    		VertexLabel book = schema.buildVertexLabel('book').add()
+    		VertexLabel meal = schema.buildVertexLabel('meal').add()
+    		VertexLabel reviewer = schema.buildVertexLabel('reviewer').add()
                 
     		// Edge Labels
-    		def authored = schema.buildEdgeLabel('authored').add()
-    		def created = schema.buildEdgeLabel('created').add()
-    		def includes = schema.buildEdgeLabel('includes').add()
-			def includedIn = schema.buildEdgeLabel('includedIn').add()
-    		def rated = schema.buildEdgeLabel('rated').add()
+    		EdgeLabel authored = schema.buildEdgeLabel('authored').add()
+    		EdgeLabel created = schema.buildEdgeLabel('created').add()
+    		EdgeLabel includes = schema.buildEdgeLabel('includes').add()
+			EdgeLabel includedIn = schema.buildEdgeLabel('includedIn').add()
+    		EdgeLabel rated = schema.buildEdgeLabel('rated').add()
                 
     		// Indexes	
-    		def ratedByStars = reviewer.buildEdgeIndex('ratedByStars', rated).direction(OUT).byPropertyKey('stars').add()
-    		def byRecipe = recipe.buildVertexIndex('byRecipe', MATERIALIZED).byPropertyKey('recipeTitle').add()
-    		def byMeal = meal.buildVertexIndex('byMeal', MATERIALIZED).byPropertyKey('mealTitle').add()
-    		def byIngredient = ingredient.buildVertexIndex('byIngredient', MATERIALIZED).byPropertyKey('iName').add()
-    		def byReviewer = reviewer.buildVertexIndex('byReviewer', MATERIALIZED).byPropertyKey('revname').add()
-		})
+    		EdgeIndex ratedByStars = reviewer.buildEdgeIndex('ratedByStars', rated).direction(OUT).byPropertyKey('stars').add()
+    		VertexIndex byRecipe = recipe.buildVertexIndex('byRecipe').materalized().byPropertyKey('recipeTitle').add()
+    		VertexIndex byMeal = meal.buildVertexIndex('byMeal').materalized().byPropertyKey('mealTitle').add()
+    		VertexIndex byIngredient = ingredient.buildVertexIndex('byIngredient').materalized().byPropertyKey('iName').add()
+    		VertexIndex byReviewer = reviewer.buildVertexIndex('byReviewer').materalized().byPropertyKey('revname').add()
+		//})
 	}
 
     public static List load(final DseGraph graph, final GraphTraversalSource g, final String dataDirectory, final int batchSize) {
