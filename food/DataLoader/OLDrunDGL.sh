2ac7ebb80d7268c9e5231c9879cc1421eacbd0f0
Lorina Poland <lorina@datastax.com>
2016-04-13 18:36:02 -0700

    minor changes

--- food/DataLoader/OLDrunDGL.sh
+++ food/DataLoader/OLDrunDGL.sh
@@ -7,11 +7,10 @@
 # GRAPHNAME defines the name of the graph used for loading. It does not have to exist prior 
 # to loading
 
-#LDR=/Users/lorinapoland/CLONES/dse-graph-loader/target/dse-graph-loader-0.1.0-SNAPSHOT-uberjar.jar
-LDR=/Users/lorinapoland/CLONES/dse-graph-loader/bin/graphloader
-TYPE=CSV
+LDR=/Users/lorinapoland/CLONES/dse-graph-loader/target/dse-graph-loader-0.1.0-SNAPSHOT-uberjar.jar
+TYPE=JDBC
 DRYRUN_SETTING=false
 INPUTFILEDIR=/Users/lorinapoland/CLONES/graph-examples/food/$TYPE/
-SCRIPTNAME='fridgeItemMappingCOMPKEY.groovy'
-GRAPHNAME=$TYPE'COMPKEYTEST'
-$LDR $INPUTFILEDIR/$SCRIPTNAME -filename INPUTFILEDIR -graph $GRAPHNAME -address localhost -dryrun DRYRUN_SETTING
+SCRIPTNAME='authorBookMapping'$TYPE'.groovy'
+GRAPHNAME=$TYPE'TEST'
+java -jar $LDR $INPUTFILEDIR/$SCRIPTNAME -filename INPUTFILEDIR -graph $GRAPHNAME -address localhost -dryrun DRYRUN_SETTING
--- food/DataLoader/runDGL.sh
+++ food/DataLoader/runDGL.sh
@@ -7,10 +7,11 @@
 # GRAPHNAME defines the name of the graph used for loading. It does not have to exist prior 
 # to loading
 
-LDR=/Users/lorinapoland/CLONES/dse-graph-loader/target/dse-graph-loader-0.1.0-SNAPSHOT-uberjar.jar
-TYPE=JDBC
+#LDR=/Users/lorinapoland/CLONES/dse-graph-loader/target/dse-graph-loader-0.1.0-SNAPSHOT-uberjar.jar
+LDR=/Users/lorinapoland/CLONES/dse-graph-loader/bin/graphloader
+TYPE=CSV
 DRYRUN_SETTING=false
 INPUTFILEDIR=/Users/lorinapoland/CLONES/graph-examples/food/$TYPE/
-SCRIPTNAME='authorBookMapping'$TYPE'.groovy'
-GRAPHNAME=$TYPE'TEST'
-java -jar $LDR $INPUTFILEDIR/$SCRIPTNAME -filename INPUTFILEDIR -graph $GRAPHNAME -address localhost -dryrun DRYRUN_SETTING
+SCRIPTNAME='fridgeItemMappingCOMPKEY.groovy'
+GRAPHNAME=$TYPE'COMPKEYTEST'
+$LDR $INPUTFILEDIR/$SCRIPTNAME -filename INPUTFILEDIR -graph $GRAPHNAME -address localhost -dryrun DRYRUN_SETTING
