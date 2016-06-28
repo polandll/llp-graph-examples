1719c8578d3b08a462e258e692fa3a33677aeb72
Lorina Poland <lorina@datastax.com>
2016-06-27 23:34:23 -0700

    mapping script gryo for dseg

--- food/Gryo/recipeMappingGRYO_DSEG.groovy
+++ food/Gryo/recipeMappingGRYO_DSEG.groovy
@@ -10,25 +10,30 @@ config create_schema: true, load_new: true
 // Define the data input source 
 // inputfiledir is the directory for the input files 
 
+//Need to specify a keymap to show how to identify vertices
+vertexKeyMap = VertexKeyMap.with('meal','name').with('ingredient','name').with('author','name').with('book','name')
+        .with('recipe','name').build();
+
+
 inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/Gryo/'
-recipeInput = Graph.file(inputfiledir + 'recipesDSEG.gryo').gryo()
+recipeInput = com.datastax.dsegraphloader.api.Graph.file(inputfiledir + 'recipesDSEG.gryo').gryo().setVertexUniqueKeys(vertexKeyMap)
 
 //Specifies what data source to load using which mapper (as defined inline)
   
-load(recipeInput.vertices().withVertexId()).asVertices {
+load(recipeInput.vertices()).asVertices {
     labelField "~label"
-    key "~id", "id"
+    key "name"
 }
 
-load(recipeInput.edges().withVertexId()).asEdges {
+load(recipeInput.edges()).asEdges {
     labelField "~label"
     outV "outV", {
         labelField "~label"
-        key "~id", "id"
+        key "name"
     }
     inV "inV", {
         labelField "~label"
-        key "~id", "id"
+        key "name"
     }
 }
 
--- food/Gryo/recipeMappingGRYO_TG.groovy
+++ food/Gryo/recipeMappingGRYO_TG.groovy
@@ -4,23 +4,25 @@ Gryo file is a binary file
 
 // CONFIGURATION
 // Configures the data loader to create the schema
-config create_schema: true, load_new: true
+config create_schema: false, load_new: true
 
 // DATA INPUT
 // Define the data input source 
 // inputfiledir is the directory for the input files 
 
-inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/Gryo/'
+inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/GRYO/'
 recipeInput = Graph.file(inputfiledir + 'recipesTG.gryo').gryo()
 
 //Specifies what data source to load using which mapper (as defined inline)
   
-load(recipeInput.vertices().withVertexId()).asVertices {
+//load(recipeInput.vertices().withVertexId()).asVertices {
+load(recipeInput.vertices()).asVertices {
     labelField "~label"
     key "~id", "id"
 }
 
-load(recipeInput.edges().withVertexId()).asEdges {
+//load(recipeInput.edges().withVertexId()).asEdges {
+load(recipeInput.edges()).asEdges {
     labelField "~label"
     outV "outV", {
         labelField "~label"
