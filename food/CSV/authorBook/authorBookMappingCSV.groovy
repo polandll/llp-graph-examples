07a37bcb2f587e149b402fdbc48aaa7f3ae4323b
Lorina Poland <lorina@datastax.com>
2017-01-06 17:08:04 -0800

    reorg

--- food/CSV/authorBook/authorBookMappingCSV.groovy
+++ food/CSV/authorBook/authorBookMappingCSV.groovy
@@ -1,7 +1,3 @@
-// Use /Users/lorinapoland/CLONES/graph-examples/DataLoader/runDGL.sh to run this script
-// Set runDGL.sh parameters to CSV before running
-// Run runDGL.sh in /Users/lorinapoland/CLONES/dse-graph-loader
-
 /* SAMPLE INPUT
 author: Julia Child|F
 book : Simca's Cuisine: 100 Classic French Recipes for Every Occasion|1972|0-394-40152-2
@@ -10,13 +6,12 @@ authorBook: Simca's Cuisine: 100 Classic French Recipes for Every Occasion|Simon
 
 // CONFIGURATION
 // Configures the data loader to create the schema
-config create_schema: true, load_new: true, load_vertex_threads: 3
+config dryrun: false, preparation: true, create_schema: true, load_new: true, load_vertex_threads: 3, schema_output: 'loader_output.txt'
 
 // DATA INPUT
 // Define the data input source (a file which can be specified via command line arguments)
-// inputfiledir is the directory for the input files that is given in the commandline
-// as the "-filename" option
-inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/CSV/'
+// inputfiledir is the directory for the input files
+inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/CSV/authorBook/'
 authorInput = File.csv(inputfiledir + "author.csv").delimiter('|')
 bookInput = File.csv(inputfiledir + "book.csv").delimiter('|')
 authorBookInput = File.csv(inputfiledir + "authorBook.csv").delimiter('|')
