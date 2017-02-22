175a885f9e041fce885e73f5a2daeaf7fec83a04
Lorina Poland <lorinapoland@gmail.com>
2017-02-22 23:59:33 +0000

    working multiCard edge scripts

--- food/TEST/multiCard/multiCardMap.groovy
+++ food/TEST/multiCard/multiCardMap.groovy
@@ -4,12 +4,13 @@ authorCity: Julia Child|Paris|1960
 
 // CONFIGURATION
 // Configures the data loader to create the schema
-config dryrun: false, preparation: true, create_schema: true, load_new: true, load_vertex_threads: 3, schema_output: 'loader_output.txt'
+config dryrun: false, preparation: true, create_schema: true, load_new: true, schema_output: 'loader_output.txt'
 
 // DATA INPUT
 // Define the data input source (a file which can be specified via command line arguments)
 // inputfiledir is the directory for the input files
-inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/Test/authorCity/'
+inputfiledir = '/home/automaton/graph-examples/food/TEST/multiCard/'
+//inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/Test/authorCity/'
 authorCityInput = File.csv(inputfiledir + "authorCity.csv").delimiter('|')
 
 //Specifies what data source to load using which mapper (as defined inline)
@@ -17,13 +18,17 @@ authorCityInput = File.csv(inputfiledir + "authorCity.csv").delimiter('|')
 load(authorCityInput).asVertices {
     label "author"
     key "author"
-    ignore "city","dateStart","dateEnd"
+    ignore "city"
+    ignore "dateStart"
+    ignore "dateEnd"
 }
 
 load(authorCityInput).asVertices {
     label "city"
     key "city"
-    ignore "author","dateStart","dateEnd"
+    ignore "author"
+    ignore "dateStart"
+    ignore "dateEnd"
 }
 
 load(authorCityInput).asEdges {
--- food/TEST/multiCard/runDGL.sh
+++ food/TEST/multiCard/runDGL.sh
@@ -9,12 +9,11 @@
 # GRAPHNAME defines the name of the graph loaded. 
 #   It does not have to exist prior to loading.
 
-VERSION=dse-graph-loader-5.0.5
-LDR=/Users/lorinapoland/CLONES/$VERSION/graphloader
-TYPE=CSV
-INPUTEXAMPLE='authorCity'
-INPUTBASEDIR='/Users/lorinapoland/CLONES/graph-examples/food'
-INPUTFILEDIR=$INPUTBASEDIR/Test/multiCard/
-SCRIPTNAME='multiCardMap.groovy'
+VERSION=dse-graph-loader-5.1.0-SNAPSHOT
+LDR=/home/automaton/$VERSION/graphloader
+INPUTEXAMPLE='multiCard'
+INPUTBASEDIR='/home/automaton/graph-examples/food/TEST'
+INPUTFILEDIR=$INPUTBASEDIR/$INPUTEXAMPLE/
+SCRIPTNAME=$INPUTEXAMPLE'Map.groovy'
 GRAPHNAME='test'$INPUTEXAMPLE
 $LDR $INPUTFILEDIR/$SCRIPTNAME -graph $GRAPHNAME -address localhost
