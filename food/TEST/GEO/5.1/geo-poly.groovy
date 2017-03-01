279d3220be7c4affe5b9741ad3e95b8480f19804
Lorina Poland <lorinapoland@gmail.com>
2017-03-01 19:22:41 +0000

    minor change to geo-poly

--- food/TEST/GEO/5.1/geo-poly.groovy
+++ food/TEST/GEO/5.1/geo-poly.groovy
@@ -1,4 +1,5 @@
 // Finds Dublin, Aachen, and Tokyo in geodeticSearch51
+// It doesn't find any cities in geodetic51
  g.V().has('location', 'point', Geo.inside(Geo.polygon(-6.26031, 53.349805, 6.083887, 50.775346, 139.691706, 35.689487)))
 // This one find Paris and Aachen for geodetic51 and finds Paris, London, and Aachen in geodeticSearch51
  g.V().has('location', 'point', Geo.inside(Geo.polygon(0,0,0,55,25,55,25,0))).values()
