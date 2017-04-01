4598c37adc817c1a6d4b50b10de4a5441729ce36
polandll <lorinapoland@gmail.com>
2017-04-01 05:48:13 +0000

    more work on fitnessTracker

--- food/TEST/fitnessTracker/fitnessTrackerMap.groovy
+++ food/TEST/fitnessTracker/fitnessTrackerMap.groovy
@@ -4,7 +4,7 @@ See users.csv, knows.csv, items.csv, meals.csv
 
 // CONFIGURATION
 // Configures the data loader to create the schema
-config create_schema: true, load_new: true
+config create_schema: false, load_new: true
 
 // DATA INPUT
 // Define the data input source (a file which can be specified via command line arguments)
@@ -16,6 +16,7 @@ users = File.csv(inputfiledir + "users.csv").delimiter('|')
 knows = File.csv(inputfiledir + "knows.csv").delimiter('|')
 items = File.csv(inputfiledir + "items.csv").delimiter('|')
 meals = File.csv(inputfiledir + "meals.csv").delimiter('|')
+meals_users = File.csv(inputfiledir + "meals_users.csv").delimiter('|')
 
 //Specifies what data source to load using which mapper (as defined inline)
 load(users).asVertices {
@@ -27,23 +28,61 @@ load(items).asVertices {
     label "item"
     key "name"
 }
-/*
+
 load(meals).asVertices {
     label "meal"
     // The vertexLabel schema for meal includes two keys:
     // partition key: type and clustering key: mealDate 
-    key type: "type", mealDate: "mealDate"
-    ignore name
+    //key type: "type", mealDate: "mealDate"
+    key "mealId"
+    ignore "name"
+    ignore "item"
+    ignore "numServings"
 }
-*/
+
+load(meals).asEdges {
+    label "includes"
+    outV "item", {
+	label "item"
+	key "name"
+        exists()
+    }
+    inV "mealId", {
+	label "meal"
+//	key type: "type", mealDate: "mealDate"
+        key "mealId"
+        exists()
+    }
+    ignore "name"
+    ignore "type"
+    ignore "mealDate"
+}
+
+load(meals_users).asEdges {
+    label "ate"
+    outV "name", {
+        label "user"
+        key "name"
+        exists()
+    }
+    inV "mealId", {
+        label "meal"
+//      key type: "type", mealDate: "mealDate"
+        key "mealId"
+        exists()
+    }
+}
+
 load(knows).asEdges {
     label "knows"
     outV "u1", {
         label "user"
         key "name"
+	exists()
     }
     inV "u2", {
         label "user"
         key "name"
+	exists()
     }
 }
--- food/TEST/fitnessTracker/schema.groovy
+++ food/TEST/fitnessTracker/schema.groovy
@@ -1,3 +1,8 @@
+system.graph('testfitnessTracker').ifNotExists().create()
+:remote config alias g testfitnessTracker.g
+schema.clear()
+schema.config().option('graph.allow_scan').set('true')
+
 // PROPERTIES
 schema.propertyKey('name').Text().single().create()
 schema.propertyKey('gender').Text().single().create()
@@ -6,15 +11,27 @@ schema.propertyKey('macroGoal').Text().single().create()
 schema.propertyKey('servingAmt').Text().single().create()
 schema.propertyKey('macro').Text().single().create()
 schema.propertyKey('calories').Text().single().create()
+schema.propertyKey('type').Text().single().create()
+schema.propertyKey('mealId').Int().single().create()
+schema.propertyKey('mealDate').Date().single().create()
 schema.propertyKey('since').Text().single().create()
+schema.propertyKey('numServings').Int().single().create()
 
 // VERTEX LABELS
 schema.vertexLabel('user').properties('name', 'gender', 'calGoal', 'macroGoal').create()
 schema.vertexLabel('item').properties('name', 'servingAmt', 'macro', 'calories').create()
+schema.vertexLabel('meal').properties('mealId', 'type', 'mealDate').create()
 
 // EDGE LABELS
-schema.edgeLabel('knows').properties('since').connection('user', 'user').create()
+schema.edgeLabel("knows").multiple().properties("since").create()
+schema.edgeLabel("knows").connection("user", "user").add()
+schema.edgeLabel("includes").multiple().properties("numServings").create()
+schema.edgeLabel("includes").connection("item", "meal").add()
+schema.edgeLabel("ate").multiple().create()
+schema.edgeLabel("ate").connection("user", "meal").add()
+
 
 // INDEXES
 schema.vertexLabel('user').index('byname').materialized().by('name').add()
 schema.vertexLabel('item').index('byname').materialized().by('name').add()
+schema.vertexLabel('meal').index('bymealId').materialized().by('mealId').add()
