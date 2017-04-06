6847e2b6c1d61cd2fd97593486a57ba7f7e5c896
polandll <lorinapoland@gmail.com>
2017-04-06 22:23:11 +0000

    fix flatmap

--- food/TEST/filter_map_flatmap/flatmapSchema.groovy
+++ food/TEST/filter_map_flatmap/flatmapSchema.groovy
@@ -5,6 +5,8 @@ schema.config().option('graph.allow_scan').set('true')
 
 schema.propertyKey('name').Text().ifNotExists().create()
 schema.propertyKey('cuisine').Text().ifNotExists().create()
+schema.propertyKey('recipeId').Int().ifNotExists().create()
 
-schema.vertexLabel('recipe').properties('name','cuisine').create()
-//schema.vertexLabel('recipe').index('byname').materialized().by('name').add()
+schema.vertexLabel('recipe').properties('recipeId','name','cuisine').create()
+schema.vertexLabel('recipe').index('byname').materialized().by('name').add()
+schema.vertexLabel('recipe').index('byId').materialized().by('recipeId').add()
