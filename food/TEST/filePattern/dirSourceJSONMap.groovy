b12b55f708b7a326acc41a7202a3a1cb9e59851b
Lorina Poland <lorina@datastax.com>
2017-06-14 16:12:12 -0700

    add filePattern example

---
+++ food/TEST/filePattern/dirSourceJSONMap.groovy
@@ -0,0 +1,21 @@
+/* SAMPLE INPUT
+{"name":"Julia Child","gender":"F"}
+*/
+
+// CONFIGURATION
+// Configures the data loader to create the schema
+config dryrun: false, preparation: true, create_schema: true, load_new: true, schema_output: 'loader_output.txt'
+
+// DATA INPUT
+// Define the data input source (a file which can be specified via command line arguments)
+// inputfiledir is the directory for the input files
+
+inputfiledir = '/home/automaton/graph-examples/food/MISC-graphloader/dirSource/data_json'
+personInput = File.directory(inputfiledir)
+
+//Specifies what data source to load using which mapper (as defined inline)
+  
+load(personInput).asVertices {
+    label "author"
+    key "name"
+}
---
+++ food/TEST/filePattern/schema.groovy
@@ -0,0 +1,13 @@
+system.graph('testFilePatJson').ifNotExists().create()
+:remote config alias g testFilePatJSON.g
+schema.config().option('graph.allow_scan').set('true')
+
+// SCHEMA
+// PROPERTIES
+schema.propertyKey('id').Text().ifNotExists().create()
+schema.propertyKey('name').Text().ifNotExists().create()
+schema.propertyKey('gender').Text().ifNotExists().create()
+// VERTEX LABELS
+schema.vertexLabel('person').properties('id', 'name', 'gender').ifNotExists().create()
+// INDEXES
+schema.vertexLabel("person").index("byid").materialized().by("id").add()
