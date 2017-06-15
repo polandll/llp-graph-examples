77a97b4feaf644f13f4cdb535bd35e178d483c4c
Lorina Poland <lorina@datastax.com>
2017-06-15 12:47:38 -0700

    compacting filePattern scripts

--- food/TEST/filePattern/filePatternMap.groovy
+++ food/TEST/filePattern/filePatternMap.groovy
@@ -1,18 +1,22 @@
 /* SAMPLE INPUT
+CSV:
 id|name|gender
 001|Julia Child|F
+JSON:
+{"name":"Julia Child","gender":"F"}
 */
 
 // CONFIGURATION
 // Configures the data loader to create the schema
-config dryrun: false, preparation: true, create_schema: true, load_new: true, schema_output: 'loader_output.txt'
+config dryrun: false, preparation: true, create_schema: false, load_new: true, schema_output: 'loader_output.txt'
 
 // DATA INPUT
 // Define the data input source (a file which can be specified via command line arguments)
 // inputfiledir is the directory for the input files
 
-inputfiledir = '/home/automaton/graph-examples/food/TEST/filePattern/data'
-personInput = File.directory(inputfiledir).fileMatches("person*.csv").delimiter('|').header('id','name','gender')
+inputfiledir = '/home/automaton/graph-examples/food/TEST/filePattern'
+inputfileCSV = inputfiledir+'/data'
+personInput = File.directory(inputfileCSV).fileMatches("person*.csv").delimiter('|').header('id','name','gender')
 
 //Specifies what data source to load using which mapper (as defined inline)
   
--- food/TEST/filePattern/schema.groovy
+++ food/TEST/filePattern/schema.groovy
@@ -1,7 +1,51 @@
-//system.graph('testFilePatJSON').ifNotExists().create()
-//:remote config alias g testFilePatJSON.g
-system.graph('testFilePatMod2').ifNotExists().create()
-:remote config alias g testFilePatMod2.g
+// CSV
+system.graph('testFilePatCSV').ifNotExists().create()
+:remote config alias g testFilePatCSV.g
+schema.config().option('graph.allow_scan').set('true')
+
+// SCHEMA
+// PROPERTIES
+schema.propertyKey('id').Text().ifNotExists().create()
+schema.propertyKey('name').Text().ifNotExists().create()
+schema.propertyKey('gender').Text().ifNotExists().create()
+// VERTEX LABELS
+schema.vertexLabel('person').properties('id', 'name', 'gender').ifNotExists().create()
+// INDEXES
+schema.vertexLabel("person").index("byid").materialized().by("id").add()
+
+// JSON
+system.graph('testFilePatJSON').ifNotExists().create()
+:remote config alias g testFilePatJSON.g
+schema.config().option('graph.allow_scan').set('true')
+
+// SCHEMA
+// PROPERTIES
+schema.propertyKey('id').Text().ifNotExists().create()
+schema.propertyKey('name').Text().ifNotExists().create()
+schema.propertyKey('gender').Text().ifNotExists().create()
+// VERTEX LABELS
+schema.vertexLabel('person').properties('id', 'name', 'gender').ifNotExists().create()
+// INDEXES
+schema.vertexLabel("person").index("byid").materialized().by("id").add()
+
+// MATCH MULTIPLE FILENAME PATTERNS
+system.graph('testFilePatMULT').ifNotExists().create()
+:remote config alias g testFilePatMULT.g
+schema.config().option('graph.allow_scan').set('true')
+
+// SCHEMA
+// PROPERTIES
+schema.propertyKey('id').Text().ifNotExists().create()
+schema.propertyKey('name').Text().ifNotExists().create()
+schema.propertyKey('gender').Text().ifNotExists().create()
+// VERTEX LABELS
+schema.vertexLabel('person').properties('id', 'name', 'gender').ifNotExists().create()
+// INDEXES
+schema.vertexLabel("person").index("byid").materialized().by("id").add()
+
+// MATCH RANGE PATTERN
+system.graph('testFilePatRANGE').ifNotExists().create()
+:remote config alias g testFilePatRANGE.g
 schema.config().option('graph.allow_scan').set('true')
 
 // SCHEMA
