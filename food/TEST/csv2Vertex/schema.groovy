92f27c8764d6bd56bbc745445fca72e1152db38f
Lorina Poland <lorina@datastax.com>
2017-06-29 17:00:06 -0700

    add schema for csv2Vertex

---
+++ food/TEST/csv2Vertex/schema.groovy
@@ -0,0 +1,8 @@
+system.graph('testCsv2Vertex').ifNotExists().create()
+:remote config alias g testCsv2Vertex.g
+schema.config().option('graph.allow_scan').set('true')
+
+schema.propertyKey('name').Text().single().create()
+schema.propertyKey('gender').Text().single().create()
+schema.vertexLabel('chef').properties('name','gender').create()
+schema.vertexLabel('chef').index('byName').materialized().add()
