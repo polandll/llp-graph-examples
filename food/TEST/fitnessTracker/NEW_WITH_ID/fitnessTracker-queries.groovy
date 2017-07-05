6ec721b6ba91cfccda7b3db97c4198296b0f9ee9
Lorina Poland <lorina@datastax.com>
2017-07-05 15:26:27 -0700

    change user to person in queries

--- food/TEST/fitnessTracker/NEW_WITH_ID/fitnessTracker-queries.groovy
+++ food/TEST/fitnessTracker/NEW_WITH_ID/fitnessTracker-queries.groovy
@@ -3,9 +3,14 @@ schema.config().option('graph.allow_scan').set('true')
 
 // Get all the graph vertices and edges
 g.V()
+g.E()
+
+// Get values 
+g.V().valueMap()
+g.E().valueMap()
 
 // What is the food that two people ate that was in common? Here the answer is that Lori and Lisa both had a taco.
-g.V().has('user','name','Lori').
+g.V().has('person','name','Lori').
     out('ate').in('includes').as('food').
     out('includes').in("ate").
     has('name',neq('Lori')).as('person').
@@ -13,17 +18,17 @@ g.V().has('user','name','Lori').
         by('name').by('name')
 
 // Copy Lisa's meal on 2017-03-30 (lunch) to my meals. (make an edge from me to that meal)
-g.V().has('user','name','Lori').as('a').
+g.V().has('person','name','Lori').as('a').
 out('knows').out('ate').as('b').
 addE('ate').from('a').to('b')
 
 // What did lunch include on 2017-03-31?
-g.V().has('user','name','Lori').
+g.V().has('person','name','Lori').
 out('ate').has('mealDate','2017-03-31').
 in('includes').values('name')
 
 // How many calories was that lunch?
-g.V().has('user','name','Lori').
+g.V().has('person','name','Lori').
 out('ate').has('mealDate','2017-03-31').
 in('includes').values('calories').
 sum()
