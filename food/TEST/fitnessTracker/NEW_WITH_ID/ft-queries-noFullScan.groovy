3e30d9adb2b40e2a74ea6b07bdd892c3dc9c7de5
Lorina Poland <lorina@datastax.com>
2017-07-06 17:27:57 -0700

    comment added

--- food/TEST/fitnessTracker/NEW_WITH_ID/ft-queries-noFullScan.groovy
+++ food/TEST/fitnessTracker/NEW_WITH_ID/ft-queries-noFullScan.groovy
@@ -24,6 +24,8 @@ g.V().match(
         __.as('food_item').in('includes').in('ate').has('person','name','Lisa').as('second_person')).
   select('first_person','second_person','food_item').by('name').dedup()
 
+// NEED TO CONSIDER HOW TO ONLY CREATE ONE ate EDGE FOR EACH PERSON FOR EACH MEAL
+// THIS QUERY WILL CREATE MULTIPLE EDGES WITH THE SAME INFO
 // Copy Lisa's meal on 2017-03-30 (lunch) to my meals. (make an edge from me to that meal)
 g.V().has('person','name','Lori').as('copier').
   out('knows').has('name','Lisa').out('ate').
