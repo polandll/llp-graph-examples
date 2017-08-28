dfff406aedca20bb0fccca56cd7768e1be3c629d
Lorina Poland <lorina@datastax.com>
2017-08-28 12:41:33 -0700

    mod multi-meta for csv

---
+++ food/MISC-graphloader/vertPropJSON/mappingMultiMetaCSV.groovy
@@ -0,0 +1,32 @@
+/* SAMPLE INPUT
+name|gender|value|since
+Jane Doe|F|Gold Badge|2011
+Jane Doe|F|Silver Badge|2005
+ */
+
+// CONFIGURATION
+// Configures the data loader to create the schema
+config dryrun: false, preparation: true, create_schema: true, load_new: true, load_vertex_threads: 3, schema_output: 'loader_output.txt'
+
+// DATA INPUT
+// Define the data input source (a file which can be specified via command line arguments)
+// inputfiledir is the directory for the input files
+inputfiledir = '/Users/lorinapoland/CLONES/graph-examples/food/MISC-graphloader/vertPropJSON/'
+reviewerInput = File.csv(inputfiledir + "reviewerMultiMeta.csv").delimiter('|')
+
+//Specifies what data source to load using which mapper (as defined inline)
+reviewerInput = reviewerInput.transform {
+  badge1 = [
+    "value": it.remove("value"),
+    "since": it.remove("since") ]
+  it["badge"] = [badge1]
+  it
+}
+
+load(reviewerInput).asVertices{
+	label "reviewer"
+	key "name"
+	vertexProperty "badge", {
+		value "value"
+	}
+}
