cf0a91524e9b94173b905c85d27f739d1f548d9b
polandll <lorinapoland@gmail.com>
2017-08-31 22:35:55 +0000

    schema for jdbc change

---
+++ food/JDBC/schema.groovy
@@ -0,0 +1,16 @@
+system.graph('jdbcmysql').ifNotExists().create()
+:remote config alias g jdbcmysql.g
+schema.clear()
+schema.config().option('graph.allow_scan').set('true')
+
+schema.propertyKey('name').Text().create()
+schema.propertyKey('gender').Text().create()
+schema.propertyKey('year').Int().create()
+schema.propertyKey('isbn').Text().create()
+schema.propertyKey('bname').Text().create()
+schema.propertyKey('aname').Text().create()
+
+schema.vertexLabel('author').properties('name', 'gender').create()
+schema.vertexLabel('book').properties('name','year','isbn').create()
+
+schema.edgeLabel('authored').connection('author','book').ifNotExists().create()
