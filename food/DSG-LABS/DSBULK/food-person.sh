027a3d76c24c1d8acd006a690be998f868730fc2
Lorina Poland <lorinapoland@gmail.com>
2019-07-29 11:44:10 -0700

    Update path to data file

--- food/DSG-LABS/DSBULK/food-person.sh
+++ food/DSG-LABS/DSBULK/food-person.sh
@@ -1,2 +1,2 @@
 bulk_version="1.2.0"
-dsbulk-$dsbulk_version/bin/dsbulk load -url /home/automaton/graph-examples-new/food/DSE6.8/data/vertices/food.person.csv -k food -t person -h localhost -header true
+dsbulk-$dsbulk_version/bin/dsbulk load -url /home/automaton/DATA/CQL_CSV/food.person.csv -k food -t person -h localhost -header true
