dcdfe6295e2beb626ac12d49d5695a4c0fb3ae50
polandll <lorinapoland@gmail.com>
2019-07-31 12:40:36 -0700

    add bash prompt

--- food/DSG-LABS/DSBULK/food-person.sh
+++ food/DSG-LABS/DSBULK/food-person.sh
@@ -1,3 +1,11 @@
-repoDir="/home/automaton/graph-examples/food/DATA/CQL_CSV
+#!/bin/bash
+
+#**********************************
+# food-person.sh
+# Use dsbulk to add person data to graph
+# Lorina Poland
+#**********************************
+
+repoDir="/home/automaton/graph-examples/food/DATA/CQL_CSV"
 
 dsbulk load -url $repoDir/person.csv -k food -t person -h localhost -header true
