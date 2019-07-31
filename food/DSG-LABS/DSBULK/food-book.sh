f5455c2413e274a893a6e7f835df9197d5af961e
polandll <lorinapoland@gmail.com>
2019-07-31 12:50:19 -0700

    fix dsbulk book

--- food/DSG-LABS/DSBULK/food-book.sh
+++ food/DSG-LABS/DSBULK/food-book.sh
@@ -1,2 +1,11 @@
-bulk_version="1.2.0"
-dsbulk-$dsbulk_version/bin/dsbulk load -url /home/automaton/graph-examples-new/food/DSE6.8/data/vertices/food.book.csv -k food -t book -h localhost -header true
+#!/bin/bash
+
+#**********************************
+# food-book.sh
+# Use dsbulk to add book data to graph
+# Lorina Poland
+#**********************************
+
+repoDir="/home/automaton/graph-examples/food/DATA/CQL_CSV"
+
+dsbulk load --schema.keyspace food_cql --schema.table book -url $repoDir/book.csv -delim '|' -header true --schema.allowMissingFields true --schema.mapping '0=book_id, 1=name, 2=publish_year 3=isbn'
