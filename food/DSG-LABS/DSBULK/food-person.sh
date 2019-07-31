77972bd696475d0a56a5b4b163e8efcc27537cce
polandll <lorinapoland@gmail.com>
2019-07-31 12:34:02 -0700

    dsbulk changes

--- food/DSG-LABS/DSBULK/food-person.sh
+++ food/DSG-LABS/DSBULK/food-person.sh
@@ -1,2 +1,2 @@
-bulk_version="1.2.0"
-dsbulk-$dsbulk_version/bin/dsbulk load -url /home/automaton/DATA/CQL_CSV/food.person.csv -k food -t person -h localhost -header true
+dsbulk_version="1.2.0"
+dsbulk-$dsbulk_version/bin/dsbulk load -url /home/automaton/DATA/CQL_CSV/person.csv -k food -t person -h localhost -header true
