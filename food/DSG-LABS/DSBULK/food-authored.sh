7cbb93328f2df4c8497e204ce24f9c31454d479f
polandll <lorinapoland@gmail.com>
2019-07-31 15:51:53 -0700

    dsbulk for authored

---
+++ food/DSG-LABS/DSBULK/food-authored.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#**********************************
+# food-book.sh
+# Use dsbulk to add book data to graph
+# Lorina Poland
+#**********************************
+
+repoDir="/home/automaton/graph-examples/food/DATA/CQL_CSV"
+
+dsbulk load -k food_cql -t person_authored_book -url $repoDir/person_authored_book.csv -m '0=person_id, 1=person_name, 2=book_id, 3=book_name' -delim '|' -header true --schema.allowMissingFields true
