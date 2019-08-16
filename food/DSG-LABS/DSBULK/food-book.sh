1889171f29e79555480495dea1eeb81d37359b46
polandll <lorinapoland@gmail.com>
2019-08-16 15:45:50 -0700

    change dir for dsbulk csv files

--- food/DSG-LABS/DSBULK/food-book.sh
+++ food/DSG-LABS/DSBULK/food-book.sh
@@ -6,6 +6,6 @@
 # Lorina Poland
 #**********************************
 
-repoDataDir="/home/automaton/graph-examples/food/DATA/CQL_CSV"
+repoDataDir="/home/automaton/graph-examples/food/DATA/CSV_JSON/vertices"
 
 dsbulk load --schema.keyspace food_cql --schema.table book -url $repoDataDir/book.csv -delim '|' -header true --schema.allowMissingFields true --schema.mapping '0=book_id, 1=name, 2=publish_year, 3=isbn'
--- food/DSG-LABS/DSBULK/food-person.sh
+++ food/DSG-LABS/DSBULK/food-person.sh
@@ -6,6 +6,6 @@
 # Lorina Poland
 #**********************************
 
-repoDataDir="/home/automaton/graph-examples/food/DATA/CQL_CSV"
+repoDataDir="/home/automaton/graph-examples/food/DATA/CSV_JSON/vertices"
 
 dsbulk load --schema.keyspace food_cql --schema.table person -url $repoDataDir/person.csv -delim '|' -header true --schema.allowMissingFields true
