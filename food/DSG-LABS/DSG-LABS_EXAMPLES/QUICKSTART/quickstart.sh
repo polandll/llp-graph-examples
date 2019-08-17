e88d78987bcfef1d58a1b763b7c979612014ce7e
polandll <lorinapoland@gmail.com>
2019-08-16 18:00:37 -0700

    always check your work

--- food/DSG-LABS/DSG-LABS_EXAMPLES/QUICKSTART/quickstart.sh
+++ food/DSG-LABS/DSG-LABS_EXAMPLES/QUICKSTART/quickstart.sh
@@ -9,11 +9,10 @@
 repoGremlinDir="/home/automaton/graph-examples/food/DSG-LABS/GREMLIN"
 
 cat $repoGremlinDir/0_create_graph.gremlin | dse gremlin-console;
-cat $repoGremlinDir/remoteQS.gremlin /
-$repoGremlinDir/create_VLs.gremlin /		
-$repoGremlinDir/create_ELs.gremlin /
-$repoGremlinDir/create_table_recipe.cql /	
-$repoGremlinDir/insert_3persons.gremlin /
-$repoGremlinDir/insert_3books.gremlin /
-$repoGremlinDir/insert_2edges.gremlin | dse gremlin-console
+cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/create_VLs.gremlin | dse gremlin-console; 
+cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/create_ELs.gremlin  | dse gremlin-console;
+cqlsh -f $repoGremlinDir/create_table_recipe.cql; 
+cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/insert_3persons.gremlin | dse gremlin-console;
+cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/insert_3books.gremlin | dse gremlin-console;
+cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/insert_2edges.gremlin | dse gremlin-console;
 
