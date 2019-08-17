46344eeaa1d44492e25c8bb3b3ce7714b291e288
polandll <lorinapoland@gmail.com>
2019-08-16 17:42:47 -0700

    add quickstart.sh

---
+++ food/DSG-LABS/DSG-LABS_EXAMPLES/QUICKSTART/quickstart.sh
@@ -0,0 +1,10 @@
+cat /
+0_create_graph.gremlin /		
+create_VLs.gremlin /		
+create_ELs.gremlin /
+create_table_recipe.cql /	
+insert_3persons.gremlin /
+insert_3books.gremlin /
+insert_2edges.gremlin /
+| dse gremlin-console
+
