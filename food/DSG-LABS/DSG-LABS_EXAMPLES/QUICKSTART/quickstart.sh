4a6e58c03390ef45ba409516e33902c1d390a3e6
polandll <lorinapoland@gmail.com>
2019-08-16 17:55:04 -0700

    cosmetics

--- food/DSG-LABS/DSG-LABS_EXAMPLES/QUICKSTART/quickstart.sh
+++ food/DSG-LABS/DSG-LABS_EXAMPLES/QUICKSTART/quickstart.sh
@@ -6,16 +6,14 @@
 # Lorina Poland
 #**********************************
 
+repoGremlinDir="/home/automaton/graph-examples/food/DSG-LABS/GREMLIN"
 
-cd /home/automaton/graph-examples/food/DSG-LABS/GREMLIN; 
-cat 0_create_graph.gremlin | dse gremlin-console;
-cat /
-remoteQS.gremlin /
-create_VLs.gremlin /		
-create_ELs.gremlin /
-create_table_recipe.cql /	
-insert_3persons.gremlin /
-insert_3books.gremlin /
-insert_2edges.gremlin /
-| dse gremlin-console
+cat $repoGremlinDir/0_create_graph.gremlin | dse gremlin-console;
+cat $repoGremlinDir/remoteQS.gremlin /
+$repoGremlinDir/create_VLs.gremlin /		
+$repoGremlinDir/create_ELs.gremlin /
+$repoGremlinDir/create_table_recipe.cql /	
+$repoGremlinDir/insert_3persons.gremlin /
+$repoGremlinDir/insert_3books.gremlin /
+$repoGremlinDir/insert_2edges.gremlin | dse gremlin-console
 
