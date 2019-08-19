7fd5f768b10b6fccb20a21d6a34f2e38272ae2ac
polandll <lorinapoland@gmail.com>
2019-08-19 11:48:33 -0700

    fix to quickstart and some notes

--- food/DSG-LABS/DSG-LABS_EXAMPLES/QUICKSTART/quickstart.sh
+++ food/DSG-LABS/DSG-LABS_EXAMPLES/QUICKSTART/quickstart.sh
@@ -2,23 +2,12 @@
 
 #**********************************
 # quickstart.sh
-# QuickStart code
-# Lorina Poland
+# QuickStart code script for public users
+# Author: Lorina Poland
 #**********************************
 
 repoGremlinDir="/home/automaton/graph-examples/food/DSG-LABS/GREMLIN"
 
-#cat $repoGremlinDir/0_create_graph_QS.gremlin | dse gremlin-console;
-#cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/create_VLs.gremlin | dse gremlin-console; 
-#cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/create_ELs.gremlin  | dse gremlin-console;
-#cqlsh -f $repoGremlinDir/create_table_recipe.cql; 
-#cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/insert_3persons.gremlin | dse gremlin-console;
-#cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/insert_3books.gremlin | dse gremlin-console;
-#cat $repoGremlinDir/remoteQS.gremlin $repoGremlinDir/insert_2edges.gremlin | dse gremlin-console;
-
-
-
-#********** why doesn't this work?????? ************
 cat $repoGremlinDir/0_create_graph_QS.gremlin | dse gremlin-console;
 cat  $repoGremlinDir/remoteQS.gremlin \
  $repoGremlinDir/create_VLs.gremlin \
