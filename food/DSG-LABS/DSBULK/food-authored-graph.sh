6e283f1cf2dd7a81431dc9b442e78f84ecc5f305
polandll <lorinapoland@gmail.com>
2019-09-03 11:59:56 -0700

    test new dsbulk graph options

---
+++ food/DSG-LABS/DSBULK/food-authored-graph.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#**********************************
+# food-book.sh
+# Use dsbulk to add book data to graph
+# Lorina Poland
+#**********************************
+
+repoDataDir="/home/automaton/graph-examples/food/DATA/CSV_JSON/edges"
+
+dsbulk load -k food -e authored -from person -to book -url $repoDataDir/person_authored_book.csv -m '0=person_id, 1=person_name, 2=book_id, 3=book_name' -delim '|' -header true --schema.allowMissingFields true
