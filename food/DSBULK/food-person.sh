df0809cca7bdb91b701a0e99b7686b89ee870855
polandll <lorinapoland@gmail.com>
2019-09-09 15:34:15 -0700

    typo

--- food/DSBULK/food-person.sh
+++ food/DSBULK/food-person.sh
@@ -10,4 +10,4 @@ dsbulkBinDir="/home/automaton/dsbulk-1.3.4/bin"
 repoDataDir="/home/automaton/graph-examples/food/DATA/CSV/vertices"
 ks="food"
 
-$dsulkBinDir/dsbulk load --schema.keyspace $ks --schema.table person -url $repoDataDir/person.csv -delim '|' -header true --schema.allowMissingFields true
+$dsbulkBinDir/dsbulk load --schema.keyspace $ks --schema.table person -url $repoDataDir/person.csv -delim '|' -header true --schema.allowMissingFields true
